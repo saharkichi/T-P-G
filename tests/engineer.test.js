9af32e71b86d9f6da024b6c0e40caa5f49d62420
saharkichi <saharkichi@gmail.com>
2022-06-21 16:09:25 -0400

    engineerTestDone

---
+++ tests/engineer.test.js
@@ -0,0 +1,26 @@
+//engineer test
+//input Github for engineer
+const Engineer = require ("../lib/Engineer.js");
+describe("GitHub", () => {
+  it("Can set GitHub account via constructor", () => {
+    const testValue = "GitHubUser";
+    const n = new Engineer("Dany", 999, "dany@email.com", testValue);
+    expect(n.GitHub).toBe(testValue);
+   });
+
+  describe("getRole", () => {
+  it("should return \"Engineer\"", () => {
+    const testValue = "Engineer";
+    const n = new Engineer("Dany", 999, "dany@email.com", "GitHubUser");
+    expect(n.getRole()).toBe(testValue);
+  });
+  });
+  //gitHub input
+  describe ("getGitHub", () => {
+  it("Can get GitHub username via getGitHub()", () => {
+    const testValue = "GitHubUser";
+    const n = new Engineer("Dany", 999, "email@email.com", testValue);
+    expect(n.getGitHub()).toBe(testValue);
+  });  
+  });
+});
